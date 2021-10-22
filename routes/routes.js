62349e21744ea0592c27b2a58faf965fac88feba
moraisp2 <pedro.morais2@vodafone.com>
2021-10-22 18:55:48 +0100

    G8F1

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,7 @@ import group0 from '../controllers/group0.js'
 import group2 from '../controllers/group2.js'
 import group3 from '../controllers/group3.js'
 import group6 from '../controllers/group6.js'
+import group8 from '../controllers/group8.js'
 import group10 from '../controllers/group10.js'
 //import ...
 
@@ -12,6 +13,7 @@ export default {
     group2,
     group3,
     group6,
+    group8,
     group10
     // ...
 }
