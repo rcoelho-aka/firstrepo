7adb7e0981224bfb10ddb295ee4e62ad72a486bd
Luis Neves <Luis.Neves01@vodafone.com>
2021-10-22 17:54:11 +0100

    G2F1 new route

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,12 @@
 // Import controllers here
 import group0 from '../controllers/group0.js'
+import group2 from '../controllers/group2.js'
 //import ...
 
 
 // Declare routes for imported controllers
 export default {
-    group0
+    group0,
+    group2
     // ...
 }
