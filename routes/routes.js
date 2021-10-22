7ac4bc6abe65fc612c341b28315ceb10b64b7398
Ricardo Almeida <ricardo.almeida2@vodafone.com>
2021-10-22 18:03:53 +0100

    G5F1 - Change Routes.

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,12 @@
 // Import controllers here
 import group0 from '../controllers/group0.js'
+import group5 from '../controllers/group5.js'
 //import ...
 
 
 // Declare routes for imported controllers
 export default {
     group0
+    group5
     // ...
 }
