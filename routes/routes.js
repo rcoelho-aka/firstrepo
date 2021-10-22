eb3fc15066a5b3a959601f1e87bc233c2bdb4f21
ferrelm <31996387+ferrelm@users.noreply.github.com>
2021-10-22 18:07:41 +0100

    G9F1 routesG9

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,12 @@
 // Import controllers here
 import group0 from '../controllers/group0.js'
+import group9 from '../controllers/group9.js'
 //import ...
 
 
 // Declare routes for imported controllers
 export default {
     group0
+    group9
     // ...
 }
