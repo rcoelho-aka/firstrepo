0df10fdf5bc2040fb6e5ffb6ee11b0bef86c4e79
Hugo Gonçalves <hugo.goncalves@vodafone.com>
2021-10-22 17:53:15 +0100

    commit routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,11 @@
 // Import controllers here
 import group0 from '../controllers/group0.js'
 //import ...
-
+import group6 from '../controllers/group6.js'
 
 // Declare routes for imported controllers
 export default {
-    group0
+    group0,
+    group6
     // ...
 }
