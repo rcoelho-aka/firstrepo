e1e04eee19a325b43144b44020513f11e9f268e7
moraisp2 <pedro.morais2@vodafone.com>
2021-10-22 17:07:08 +0100

    Group8 routing added

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,12 @@
 // Import controllers here
 import group0 from '../controllers/group0.js'
+import group8 from '../controllers/group8.js'
 //import ...
 
 
 // Declare routes for imported controllers
 export default {
-    group0
+    group0,
+    group8
     // ...
 }
