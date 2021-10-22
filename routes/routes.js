4c83d9d7b892f8185ea2abe7926abb429150c973
moraisp2 <pedro.morais2@vodafone.com>
2021-10-22 18:35:59 +0100

    G8F1

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,12 @@
 // Import controllers here
 import group0 from '../controllers/group0.js'
 import group8 from '../controllers/group8.js'
-//import ...
-import group6 from '../controllers/group6.js'
+
+
 
 // Declare routes for imported controllers
 export default {
     group0,
-    group8,
-    group6
+    group8
     // ...
 }
